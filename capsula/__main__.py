6b96f3c06a3ba287d3fc1d57d67c8d4b80686842
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2023-06-24 11:02:26 +0900

    Add directory option and pydantic dependency

--- capsula/__main__.py
+++ capsula/__main__.py
@@ -1,11 +1,25 @@
+from pathlib import Path
+
 import click
 
 
 @click.group()
-def main() -> None:
+@click.option(
+    "--directory",
+    "-C",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True, path_type=Path),
+)
+@click.pass_context
+def main(ctx: click.Context, directory: Path) -> None:
     click.echo("main")
+    ctx.ensure_object(dict)
+
+    ctx.obj["directory"] = directory
 
 
 @main.command()
-def freeze() -> None:
+@click.pass_context
+def freeze(ctx: click.Context) -> None:
+    """Freeze the environment into a file."""
     click.echo("freeze")
+    click.echo(f"directory: {ctx.obj['directory']}")
