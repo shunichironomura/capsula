de802da40a4139f552ae80b6c666ae02942fae72
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2023-07-14 00:25:45 +0900

    Add capsule name setting and getting methods

--- capsula/globalvars.py
+++ capsula/globalvars.py
@@ -3,14 +3,28 @@ from pathlib import Path
 
 
 def set_capsule_dir(path: Path | str) -> None:
-    """Set the capsule directory used by the monitor CLI."""
+    """Set the capsule directory used by the CLI."""
     os.environ["CAPSULA_CAPSULE_DIR"] = str(path)
 
 
 def get_capsule_dir() -> Path:
-    """Get the capsule directory used by the monitor CLI."""
+    """Get the capsule directory used by the CLI."""
     try:
         return Path(os.environ["CAPSULA_CAPSULE_DIR"])
     except KeyError as e:
-        msg = "CAPSULA_CAPSULE_DIR is not set. Run `capsula monitor <args>` to set it."
-        raise RuntimeError(msg) from e
+        msg = "CAPSULA_CAPSULE_DIR is not set."
+        raise ValueError(msg) from e
+
+
+def set_capsule_name(name: str) -> None:
+    """Set the capsule name used by the CLI."""
+    os.environ["CAPSULA_CAPSULE_NAME"] = name
+
+
+def get_capsule_name() -> str:
+    """Get the capsule name used by the CLI."""
+    try:
+        return os.environ["CAPSULA_CAPSULE_NAME"]
+    except KeyError as e:
+        msg = "CAPSULA_CAPSULE_NAME is not set."
+        raise ValueError(msg) from e
