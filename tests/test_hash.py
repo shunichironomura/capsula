71e289ead0f5931d34e1c30ff509f1d38ae1a019
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2023-07-15 16:35:29 +0900

    Refactor hash values for better readability

--- tests/test_hash.py
+++ tests/test_hash.py
@@ -16,9 +16,11 @@ _HELLO_WORLD_HASH = {
     # Checked with `echo -n "Hello world!" | openssl dgst -sha3-256`
     HashAlgorithm.SHA3_256: "d6ea8f9a1f22e1298e5a9506bd066f23cc56001f5d36582344a628649df53ae8",
     # Checked with `echo -n "Hello world!" | openssl dgst -sha3-384`
-    HashAlgorithm.SHA3_384: "f9210511d0b2862bdcb672daa3f6a4284576ccb24d5b293b366b39c24c41a6918464035ec4466b12e22056bf559c7a49",
+    HashAlgorithm.SHA3_384: "f9210511d0b2862bdcb672daa3f6a4284576ccb24d5b293b"
+    "366b39c24c41a6918464035ec4466b12e22056bf559c7a49",
     # Checked with `echo -n "Hello world!" | openssl dgst -sha3-512`
-    HashAlgorithm.SHA3_512: "95decc72f0a50ae4d9d5378e1b2252587cfc71977e43292c8f1b84648248509f1bc18bc6f0b0d0b8606a643eff61d611ae84e6fbd4a2683165706bd6fd48b334",
+    HashAlgorithm.SHA3_512: "95decc72f0a50ae4d9d5378e1b2252587cfc71977e43292c8f1b84648248509f"
+    "1bc18bc6f0b0d0b8606a643eff61d611ae84e6fbd4a2683165706bd6fd48b334",
 }
 
 
