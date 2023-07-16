c5ff8d8910d1bfeff9a126d2124b9c375da9ac33
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2023-07-16 10:44:05 +0900

    Update return type in temporary_root_directory function

--- tests/utils.py
+++ tests/utils.py
@@ -2,7 +2,7 @@ import json
 import tempfile
 from contextlib import contextmanager
 from pathlib import Path
-from typing import Any, Generator
+from typing import Iterator
 
 import tomli_w
 
@@ -10,7 +10,7 @@ from capsula.config import CapsulaConfig
 
 
 @contextmanager
-def temporary_root_directory(capsula_config: CapsulaConfig) -> Generator[Path, Any, None]:
+def temporary_root_directory(capsula_config: CapsulaConfig) -> Iterator[Path]:
     with tempfile.TemporaryDirectory() as root_directory:
         with (Path(root_directory) / "capsula.toml").open("wb") as f:
             json_str = capsula_config.model_dump_json()
