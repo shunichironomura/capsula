8f31f46a028559b163259386d38538c4352faa03
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2023-07-16 11:15:07 +0900

    Add tests for pre-capture commands in capture module

--- tests/test_capture.py
+++ tests/test_capture.py
@@ -1,5 +1,7 @@
+import shlex
 from pathlib import Path
 
+import pytest
 from click.testing import CliRunner
 
 from capsula.__main__ import main
@@ -20,3 +22,42 @@ def test_capture() -> None:
         runner = CliRunner()
         result = runner.invoke(main, ["--directory", str(root_directory), "capture"])
         assert result.exit_code == 0
+
+
+@pytest.mark.parametrize(
+    ("cmd", "exit_code", "stdout", "stderr"),
+    [
+        (
+            shlex.join(["python", "-c", "import sys; sys.exit(0)"]),
+            0,
+            "",
+            "",
+        ),
+        (
+            shlex.join(["python", "-c", "import sys; sys.exit(1)"]),
+            1,
+            "",
+            "",
+        ),
+        (
+            shlex.join(["python", "-c", "import sys; sys.stdout.write('hello\\n'); sys.stderr.write('world\\n')"]),
+            0,
+            "hello\n",
+            "world\n",
+        ),
+    ],
+)
+def test_capture_pre_capture_command(cmd: str, exit_code: int, stdout: str, stderr: str) -> None:
+    capsula_config = CapsulaConfig(
+        vault_directory=Path("vault"),
+        capsule_template=r"%Y%m%d_%H%M%S",
+        capture=CaptureConfig(
+            pre_capture_commands=[cmd],
+        ),
+        monitor=MonitorConfig(),
+    )
+
+    with temporary_root_directory(capsula_config) as root_directory:
+        runner = CliRunner()
+        result = runner.invoke(main, ["--directory", str(root_directory), "capture"])
+        assert (result.exit_code == 0) == (exit_code == 0)
