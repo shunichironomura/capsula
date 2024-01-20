c1bd3353b4e6a1150e27ae7dfcd1eb12804b823a
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-01-20 23:37:19 +0900

    Add default_key method to Context class

--- capsula/context/_base.py
+++ capsula/context/_base.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from typing import Any
 
@@ -6,3 +8,7 @@ class Context(ABC):
     @abstractmethod
     def encapsulate(self) -> Any:
         raise NotImplementedError
+
+    def default_key(self) -> str | tuple[str, ...]:
+        msg = f"{self.__class__.__name__}.default_key() is not implemented"
+        raise NotImplementedError(msg)
