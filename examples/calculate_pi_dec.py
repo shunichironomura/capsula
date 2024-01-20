d4d296472b9e74dda6448d40424a80861ced2923
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-01-20 09:58:03 +0900

    Remove numpy-related codes

--- examples/calculate_pi_dec.py
+++ examples/calculate_pi_dec.py
@@ -2,9 +2,6 @@ import logging
 from pathlib import Path
 from typing import Optional
 
-import matplotlib.pyplot as plt
-import numpy as np
-
 import capsula
 
 logger = logging.getLogger(__name__)
@@ -24,32 +21,7 @@ def main(n: int, seed: Optional[int] = None) -> float:
     logger.info(f"Calculating pi with {n} samples.")
     logger.debug(f"Seed: {seed}")
 
-    rng = np.random.default_rng(seed=seed)
-    x = rng.random(n, dtype=np.float64)
-    y = rng.random(n, dtype=np.float64)
-
-    pi = float(4 * np.sum(x**2 + y**2 <= 1) / n)
-
-    # Plot the results
-    fig = plt.figure(figsize=(8, 8))
-
-    # Plot the points
-    ax = fig.add_subplot(1, 1, 1)
-    ax.scatter(x, y, s=0.01, c="k")
-    ax.set_aspect("equal")
-    ax.set_xlim(0, 1)
-    ax.set_ylim(0, 1)
-    capsule_name = capsula.get_capsule_name()
-    ax.set_title(f"Approximation of pi: {pi:.6f} ({capsule_name})")
-
-    # Plot the circle
-    theta = np.linspace(0, 2 * np.pi, 100)
-    ax.plot(np.cos(theta), np.sin(theta), c="k")
-
-    output_path = Path(__file__).parent / "pi_dec.png"
-    fig.savefig(str(output_path), dpi=300)
-
-    return pi
+    return 3.1415926535897932384626433832795028841971
 
 
 if __name__ == "__main__":
