d68e5cd1a423abef4f460adf824c2f8dc9945101
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-01-21 14:52:32 +0900

    Add PlatformContext to context module

--- capsula/context/__init__.py
+++ capsula/context/__init__.py
@@ -1,6 +1,14 @@
-__all__ = ["Context", "CwdContext", "EnvVarContext", "GitRepositoryContext", "FileContext"]
+__all__ = [
+    "Context",
+    "CwdContext",
+    "EnvVarContext",
+    "GitRepositoryContext",
+    "FileContext",
+    "PlatformContext",
+]
 from ._base import Context
 from ._cwd import CwdContext
 from ._envvar import EnvVarContext
 from ._file import FileContext
 from ._git import GitRepositoryContext
+from ._platform import PlatformContext
---
+++ capsula/context/_platform.py
@@ -0,0 +1,31 @@
+import platform as pf
+
+from ._base import Context
+
+
+class PlatformContext(Context):
+    def encapsulate(self) -> dict:
+        return {
+            "machine": pf.machine(),
+            "node": pf.node(),
+            "platform": pf.platform(),
+            "release": pf.release(),
+            "version": pf.version(),
+            "system": pf.system(),
+            "processor": pf.processor(),
+            "python": {
+                "executable_architecture": {
+                    "bits": pf.architecture()[0],
+                    "linkage": pf.architecture()[1],
+                },
+                "build_no": pf.python_build()[0],
+                "build_date": pf.python_build()[1],
+                "compiler": pf.python_compiler(),
+                "branch": pf.python_branch(),
+                "implementation": pf.python_implementation(),
+                "version": pf.python_version(),
+            },
+        }
+
+    def default_key(self) -> str:
+        return "platform"
