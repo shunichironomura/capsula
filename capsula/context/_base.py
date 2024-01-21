dfb37a472da821ef5520235f89b970c43fe42885
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-01-21 10:12:05 +0900

    Refactor Context class to inherit from CapsuleItem

--- capsula/context/_base.py
+++ capsula/context/_base.py
@@ -1,14 +1,5 @@
-from __future__ import annotations
-
-from abc import ABC, abstractmethod
-from typing import Any
+from capsula.capsule import CapsuleItem
 
 
-class Context(ABC):
-    @abstractmethod
-    def encapsulate(self) -> Any:
-        raise NotImplementedError
-
-    def default_key(self) -> str | tuple[str, ...]:
-        msg = f"{self.__class__.__name__}.default_key() is not implemented"
-        raise NotImplementedError(msg)
+class Context(CapsuleItem):
+    pass
