7db344c959029abcc1c0baef9af2bd28e32b65dc
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-01-21 14:18:06 +0900

    Add file digest function for Python versions below 3.11

--- capsula/hash.py
+++ capsula/hash.py
@@ -1,14 +1,79 @@
+from __future__ import annotations
+
 __all__ = [
     "HashAlgorithm",
     "compute_hash",
 ]
 import hashlib
+import io
 import logging
+import sys
 from enum import Enum
 from pathlib import Path
 from typing import Callable, Dict
 
 logger = logging.getLogger(__name__)
+
+if sys.version_info >= (3, 11):
+    file_digest = hashlib.file_digest
+else:
+    from typing import Protocol
+
+    class _BytesIOLike(Protocol):
+        def getbuffer(self) -> io.ReadableBuffer:
+            ...
+
+    class _FileDigestFileObj(Protocol):
+        def readinto(self, __buf: bytearray) -> int:
+            ...
+
+        def readable(self) -> bool:
+            ...
+
+    def file_digest(
+        fileobj: _BytesIOLike | _FileDigestFileObj,
+        digest: str | Callable[[], hashlib._Hash],
+        /,
+        *,
+        _bufsize: int = 2**18,
+    ):
+        """Hash the contents of a file-like object. Returns a digest object.
+
+        *fileobj* must be a file-like object opened for reading in binary mode.
+        It accepts file objects from open(), io.BytesIO(), and SocketIO objects.
+        The function may bypass Python's I/O and use the file descriptor *fileno*
+        directly.
+
+        *digest* must either be a hash algorithm name as a *str*, a hash
+        constructor, or a callable that returns a hash object.
+        """
+        # On Linux we could use AF_ALG sockets and sendfile() to archive zero-copy
+        # hashing with hardware acceleration.
+        if isinstance(digest, str):
+            digestobj = hashlib.new(digest)
+        else:
+            digestobj = digest()
+
+        if hasattr(fileobj, "getbuffer"):
+            # io.BytesIO object, use zero-copy buffer
+            digestobj.update(fileobj.getbuffer())
+            return digestobj
+
+        # Only binary files implement readinto().
+        if not (hasattr(fileobj, "readinto") and hasattr(fileobj, "readable") and fileobj.readable()):
+            raise ValueError(f"'{fileobj!r}' is not a file-like object in binary reading mode.")
+
+        # binary file, socket.SocketIO object
+        # Note: socket I/O uses different syscalls than file I/O.
+        buf = bytearray(_bufsize)  # Reusable buffer to reduce allocations.
+        view = memoryview(buf)
+        while True:
+            size = fileobj.readinto(buf)
+            if size == 0:
+                break  # EOF
+            digestobj.update(view[:size])
+
+        return digestobj
 
 
 class HashAlgorithm(Enum):
@@ -20,7 +85,7 @@ class HashAlgorithm(Enum):
     SHA3_512 = "sha3-512"
 
 
-_HASH_CONSTRUCTOR: Dict[HashAlgorithm, Callable[..., "hashlib._Hash"]] = {
+_HASH_CONSTRUCTOR: Dict[HashAlgorithm, Callable[..., hashlib._Hash]] = {
     HashAlgorithm.MD5: hashlib.md5,
     HashAlgorithm.SHA1: hashlib.sha1,
     HashAlgorithm.SHA256: hashlib.sha256,
@@ -32,10 +97,11 @@ _HASH_CONSTRUCTOR: Dict[HashAlgorithm, Callable[..., "hashlib._Hash"]] = {
 assert set(_HASH_CONSTRUCTOR.keys()) == set(HashAlgorithm)
 
 
-def compute_hash(file_path: Path, algorithm: HashAlgorithm | "hashlib._Hash") -> str:
+def compute_hash(file_path: Path, algorithm: HashAlgorithm | Callable[..., hashlib._Hash]) -> str:
     buf_size = 65536  # lets read stuff in 64kb chunks!
 
-    hash_value = _HASH_CONSTRUCTOR[algorithm]()
+    algorithm_ = _HASH_CONSTRUCTOR[algorithm] if isinstance(algorithm, HashAlgorithm) else algorithm
+    hash_value = algorithm_()
 
     with file_path.open("rb") as f:
         while True:
