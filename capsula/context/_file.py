ec0ea9a3c08afebf6c1268a7df40961c055a5b2b
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-01-21 14:47:05 +0900

    Update FileContext to handle multiple copy destinations

--- capsula/context/_file.py
+++ capsula/context/_file.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 import hashlib
 import logging
 from pathlib import Path
-from typing import Callable, Literal, overload
+from shutil import copyfile, move
+from typing import Callable, Iterable, Literal, overload
 
 from git.repo import Repo
 
@@ -20,14 +21,28 @@ class FileContext(Context):
         self,
         path: Path | str,
         *,
-        hash_algorithm: str | Callable[[], hashlib._Hash] | None,
-        copy_to: Path | str | None = None,
+        hash_algorithm: str | None,
+        copy_to: Iterable[Path | str] | Path | str | None = None,
         move_to: Path | str | None = None,
     ) -> None:
         self.path = Path(path)
         self.hash_algorithm = hash_algorithm
-        self.copy_to = None if copy_to is None else Path(copy_to)
         self.move_to = None if move_to is None else Path(move_to)
+
+        if copy_to is None:
+            self.copy_to: tuple[Path, ...] = ()
+        elif isinstance(copy_to, (str, Path)):
+            self.copy_to = (Path(copy_to),)
+        else:
+            self.copy_to = tuple(Path(p) for p in copy_to)
+
+        def normalize_copy_dst_path(p: Path) -> Path:
+            if p.is_dir():
+                return p / self.path.name
+            else:
+                return p
+
+        self.copy_to = tuple(normalize_copy_dst_path(p) for p in self.copy_to)
 
     def encapsulate(self) -> dict:
         if self.hash_algorithm is None:
@@ -36,12 +51,20 @@ class FileContext(Context):
             with self.path.open("rb") as f:
                 digest = file_digest(f, self.hash_algorithm).hexdigest()
 
-        info = {
+        info: dict = {
             "hash": {
                 "algorithm": self.hash_algorithm,
                 "digest": digest,
             },
+            "copied_to": self.copy_to,
+            "moved_to": self.move_to,
         }
+
+        for path in self.copy_to:
+            copyfile(self.path, path)
+        if self.move_to is not None:
+            move(self.path, self.move_to)
+
         return info
 
     def default_key(self) -> tuple[str, str]:
