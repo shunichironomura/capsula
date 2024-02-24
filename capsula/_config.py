38fa8e90ac475f6b673103999ea65c43724d47cc
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-02-24 23:12:18 +0900

    Add watcher and reporter construction functions

--- capsula/_config.py
+++ capsula/_config.py
@@ -5,6 +5,8 @@ from typing import TYPE_CHECKING, Any, Callable, MutableMapping
 
 from ._backport import tomllib
 from ._context import ContextBase
+from ._reporter import ReporterBase
+from ._watcher import WatcherBase
 
 if TYPE_CHECKING:
     from ._run import CapsuleParams
@@ -14,6 +16,18 @@ def _construct_context(raw_config: MutableMapping[str, Any]) -> Callable[[Capsul
     context_class_name = raw_config.pop("type")
     context_class = ContextBase.get_subclass(context_class_name)
     return context_class.default(**raw_config)
+
+
+def _construct_watcher(raw_config: MutableMapping[str, Any]) -> Callable[[CapsuleParams], WatcherBase] | WatcherBase:
+    watcher_class_name = raw_config.pop("type")
+    watcher_class = WatcherBase.get_subclass(watcher_class_name)
+    return watcher_class.default(**raw_config)
+
+
+def _construct_reporter(raw_config: MutableMapping[str, Any]) -> Callable[[CapsuleParams], ReporterBase] | ReporterBase:
+    reporter_class_name = raw_config.pop("type")
+    reporter_class = ReporterBase.get_subclass(reporter_class_name)
+    return reporter_class.default(**raw_config)
 
 
 def load_config(path: Path):
@@ -27,7 +41,7 @@ def load_config(path: Path):
     }
 
     if (pre_run := raw_config.get("pre-run")) is not None:
-        config["pre-run"]["context"] = [_construct_context(conf) for conf in pre_run.get("context", [])]
+        config["pre-run"]["context"] = list(map(_construct_context, pre_run.get("context", [])))
 
     print(config)
 
