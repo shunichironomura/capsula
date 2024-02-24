badbb23d6e64fc6b6df7563fb79813cd55ecf80c
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-02-24 23:11:53 +0900

    Refactor ReporterBase class

--- capsula/_reporter/_base.py
+++ capsula/_reporter/_base.py
@@ -1,9 +1,32 @@
-from abc import ABC, abstractmethod
+from __future__ import annotations
 
-from capsula._capsule import Capsule
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any, Callable, Final
+
+if TYPE_CHECKING:
+    from capsula._backport import Self
+    from capsula._capsule import Capsule
+    from capsula._run import CapsuleParams
 
 
 class ReporterBase(ABC):
+    _subclass_registry: Final[dict[str, type[ReporterBase]]] = {}
+
+    def __init_subclass__(cls, **kwargs: Any) -> None:
+        cls._subclass_registry[cls.__name__] = cls
+        super().__init_subclass__(**kwargs)
+
+    @classmethod
+    def get_subclass(cls, name: str) -> type[ReporterBase]:
+        return cls._subclass_registry[name]
+
     @abstractmethod
     def report(self, capsule: Capsule) -> None:
         raise NotImplementedError
+
+    @classmethod
+    def default(cls, *args: Any, **kwargs: Any) -> Callable[[CapsuleParams], Self]:
+        def callback(params: CapsuleParams) -> Self:  # type: ignore[type-var,misc] # noqa: ARG001
+            return cls(*args, **kwargs)
+
+        return callback
