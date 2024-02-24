277fd731912887d1963e4fe6b2e405a1ced4051e
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-02-24 22:40:06 +0900

    add subclass registry to the ContextBase class

--- capsula/_context/_base.py
+++ capsula/_context/_base.py
@@ -1,5 +1,17 @@
+from __future__ import annotations
+
+from typing import Any, Final
+
 from capsula._capsule import CapsuleItem
 
 
 class ContextBase(CapsuleItem):
-    pass
+    _subclass_registry: Final[dict[str, type[ContextBase]]] = {}
+
+    def __init_subclass__(cls, **kwargs: Any) -> None:
+        cls._subclass_registry[cls.__name__] = cls
+        super().__init_subclass__(**kwargs)
+
+    @classmethod
+    def get_subclass(cls, name: str) -> type[ContextBase]:
+        return cls._subclass_registry[name]
