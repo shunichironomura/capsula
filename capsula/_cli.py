3137c019f0fc5a33a1854db42aa4fec1f9e55eae
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-02-25 01:54:14 +0900

    Add enum and context-related changes to _cli.py

--- capsula/_cli.py
+++ capsula/_cli.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from pathlib import Path
 from typing import Annotated, Literal, NoReturn
 
@@ -6,6 +7,8 @@ import typer
 import capsula
 
 from ._config import load_config
+from ._context import ContextBase
+from ._run import CapsuleParams, generate_default_run_dir
 from .utils import search_for_project_root
 
 app = typer.Typer()
@@ -26,21 +29,34 @@ def run() -> NoReturn:
     raise typer.Exit
 
 
+class _PhaseForEncapsulate(str, Enum):
+    pre = "pre"
+    post = "post"
+
+
 @app.command()
 def enc(
     phase: Annotated[
-        Literal["pre", "post"],
+        _PhaseForEncapsulate,
         typer.Option(..., "--phase", "-p", help="The phase to encapsulate."),
-    ] = "pre",
+    ] = _PhaseForEncapsulate.pre,
 ) -> NoReturn:
     typer.echo("Encapsulating...")
     config = load_config(_get_default_config_path())
     enc = capsula.Encapsulator()
-    phase_key: Literal["pre-run", "post-run"] = f"{phase}-run"  # type: ignore[assignment]
+    phase_key: Literal["pre-run", "post-run"] = f"{phase.value}-run"  # type: ignore[assignment]
     contexts = config[phase_key]["context"]
     reporters = config[phase_key]["reporter"]
 
+    exec_info = None
+    run_dir = generate_default_run_dir(exec_info=exec_info)
+    params = CapsuleParams(exec_info=exec_info, run_dir=run_dir, phase=phase.value)
+
     for context in contexts:
-        enc.add_context(context)
+        enc.add_context(context if isinstance(context, ContextBase) else context(params))
+
+    capsule = enc.encapsulate()
+    for reporter in reporters:
+        (reporter if isinstance(reporter, capsula.ReporterBase) else reporter(params)).report(capsule)
 
     raise typer.Exit
