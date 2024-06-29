3cc24878257f24f5e7947caa07839c7a7b7cf145
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-06-29 14:44:03 +0900

    Remove reraise option from UncaughtExceptionWatcher

--- capsula/_watcher/_exception.py
+++ capsula/_watcher/_exception.py
@@ -20,11 +20,9 @@ class UncaughtExceptionWatcher(WatcherBase):
         name: str = "exception",
         *,
         base: type[BaseException] = Exception,
-        reraise: bool = False,
     ) -> None:
         self.name = name
         self.base = base
-        self.reraise = reraise
         self.exception: BaseException | None = None
 
     def encapsulate(self) -> ExceptionInfo:
@@ -36,10 +34,9 @@ class UncaughtExceptionWatcher(WatcherBase):
         try:
             yield
         except self.base as e:
-            logger.debug(f"UncaughtExceptionWatcher: {self.name} caught exception: {e}")
+            logger.debug(f"UncaughtExceptionWatcher: {self.name} observed exception: {e}")
             self.exception = e
-            if self.reraise:
-                raise
+            raise
 
     def default_key(self) -> tuple[str, str]:
         return ("exception", self.name)
