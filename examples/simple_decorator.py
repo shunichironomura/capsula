db7b86d3ed95e178521cd140505f1c8b25f4f30e
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-06-30 01:58:15 +0900

    Simplify example

--- examples/simple_decorator.py
+++ examples/simple_decorator.py
@@ -1,11 +1,10 @@
 import random
-from pathlib import Path
 
 import capsula
 
 
 @capsula.run()
-@capsula.context(capsula.FileContext.default(Path(__file__).parent / "pi.txt", move=True), mode="post")
+@capsula.context(capsula.FileContext.default("pi.txt", move=True), mode="post")
 def calculate_pi(n_samples: int = 1_000, seed: int = 42) -> None:
     random.seed(seed)
     xs = (random.random() for _ in range(n_samples))
@@ -20,7 +19,7 @@ def calculate_pi(n_samples: int = 1_000, seed: int = 42) -> None:
     capsula.record("pi_estimate", pi_estimate)
     print(f"Run name: {capsula.current_run_name()}")
 
-    with (Path(__file__).parent / "pi.txt").open("w") as output_file:
+    with open("pi.txt", "w") as output_file:
         output_file.write(f"Pi estimate: {pi_estimate}.")
 
 
