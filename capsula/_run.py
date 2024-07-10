0d8e43e3ee0e1f43fe8c826823ef0d79c3701bda
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-07-10 21:30:43 +0900

    Rename to FunctionContext

--- capsula/_run.py
+++ capsula/_run.py
@@ -13,7 +13,7 @@ from string import ascii_letters, digits
 from typing import TYPE_CHECKING, Any, Callable, Generic, Literal, TypeVar, Union, overload
 
 from ._backport import Concatenate, ParamSpec, Self, TypeAlias
-from ._context import ContextBase, FunctionCallContext
+from ._context import ContextBase, FunctionContext
 from ._encapsulator import Encapsulator
 from ._reporter import ReporterBase
 from ._utils import search_for_project_root
@@ -278,7 +278,7 @@ class Run(Generic[_P, _T]):
             watcher = watcher_generator(params)
             in_run_enc.add_watcher(watcher)
 
-        in_run_enc.add_context(FunctionCallContext(self._func, args, kwargs))
+        in_run_enc.add_context(FunctionContext(self._func, args, kwargs))
 
         try:
             with self, in_run_enc, in_run_enc.watch():
