45dc1f4719f6ffe21336938acc1a9f8645c43b30
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-09-23 18:09:31 +0900

    feat: Add logging of run directory in test_default_run_dir function.

--- tests/test_run.py
+++ tests/test_run.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 def test_default_run_dir() -> None:
     @capsula.run(ignore_config=True)
     def f(x: int, y: int) -> int:
+        logger.info(f"Run directory: {capsula.Run.get_current().run_dir}")
         return x + y
 
-    logger.info(f"Run directory: {f.run_dir}")
+    f(1, 2)
