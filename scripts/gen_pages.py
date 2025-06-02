72289613fe45d7de900b47916cb582ebad1e0cb4
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-06-02 11:42:12 +0900

    Adjust `gen_pages.py` to src layout

--- scripts/gen_pages.py
+++ scripts/gen_pages.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 import mkdocs_gen_files
 
-root = Path(__file__).parents[1]
+PROJECT_ROOT = Path(__file__).parents[1]
 
 
 def gen_index_page() -> None:
-    readme_path = root / "README.md"
+    readme_path = PROJECT_ROOT / "README.md"
     index_path = Path("index.md")
 
     readme_content = readme_path.read_text()
@@ -38,14 +38,53 @@ def remove_lines_with_documentation_link(content: str) -> str:
     )
 
 
+def convert_to_mkdocs_admonition(content: str) -> str:
+    """Convert the GitHub-style alert to mkdocs admonition format.
+
+    This function is used to convert the GitHub-style alert to mkdocs admonition format.
+
+    Example:
+    ```markdown
+    > [!NOTE]
+    > This is a note.
+    > This is still a note.
+    ```
+    becomes:
+    ```markdown
+    !!! note
+        This is a note.
+        This is still a note.
+    ```
+
+    """
+    lines = content.split("\n")
+
+    alert_start_line_indices = [i for i, line in enumerate(lines) if line.startswith("> [!")]
+    for start_index in alert_start_line_indices:
+        end_index = start_index + 1
+        while end_index < len(lines) and lines[end_index].startswith("> "):
+            end_index += 1
+
+        alert_lines = lines[start_index:end_index]
+        alert_type = alert_lines[0][len("> [!") : alert_lines[0].index("]")]
+        alert_content_lines = [line[2:] for line in alert_lines[1:]]
+        lines[start_index:end_index] = [
+            f"!!! {alert_type.lower()}",
+            *(f"    {line}" for line in alert_content_lines),
+        ]
+
+    return "\n".join(lines)
+
+
 def gen_ref_pages() -> None:
     nav = mkdocs_gen_files.Nav()  # type: ignore[attr-defined, no-untyped-call]
 
-    package_root = root / "capsula"
+    src_dir = PROJECT_ROOT / "src"
+    package_root = src_dir / "capsula"
 
     for path in sorted(package_root.rglob("*.py")):
-        module_path = path.relative_to(root).with_suffix("")
-        doc_path = path.relative_to(root).with_suffix(".md")
+        module_path = path.relative_to(src_dir).with_suffix("")
+        doc_path = path.relative_to(src_dir).with_suffix(".md")
         full_doc_path = Path("reference", doc_path)
 
         parts = tuple(module_path.parts)
