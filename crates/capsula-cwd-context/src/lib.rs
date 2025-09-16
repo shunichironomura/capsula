4014a89afaf3c4ee015d0eafd009cff8e0c73d83
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-09-16 23:32:05 +0900

    Rename `CwdCapture` to `CwdCaptured`

--- crates/capsula-cwd-context/src/lib.rs
+++ crates/capsula-cwd-context/src/lib.rs
@@ -8,23 +8,23 @@ use std::path::PathBuf;
 pub struct CwdContext;
 
 #[derive(Debug)]
-pub struct CwdCapture {
+pub struct CwdCaptured {
     pub cwd_abs: PathBuf,
 }
 
 impl Context for CwdContext {
-    type Output = CwdCapture;
+    type Output = CwdCaptured;
 
     fn type_name(&self) -> &'static str {
         "CwdContext"
     }
     fn run(&self, _params: &ContextParams) -> CoreResult<Self::Output> {
         let cwd_abs = std::env::current_dir()?;
-        Ok(CwdCapture { cwd_abs })
+        Ok(CwdCaptured { cwd_abs })
     }
 }
 
-impl Captured for CwdCapture {
+impl Captured for CwdCaptured {
     fn to_json(&self) -> serde_json::Value {
         json!({
             "cwd": self.cwd_abs.to_string_lossy(),
