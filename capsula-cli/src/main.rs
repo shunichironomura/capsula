b90d6279986114c7b1bc2e8caaa2e49d2b3415b9
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-09-16 21:28:21 +0900

    Add capture command

--- capsula-cli/src/main.rs
+++ capsula-cli/src/main.rs
@@ -1,25 +1,29 @@
 use clap::{Parser, Subcommand};
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(name = "capsula", bin_name = "capsula", version, about = "Capsula CLI")]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 enum Commands {
-    Run {
-        #[arg(trailing_var_arg = true)]
-        cmd: Vec<String>,
+    Capture {
+        #[arg(short, long = "context")]
+        contexts: Vec<String>,
+
+        #[arg(short, long, default_value = "json")]
+        format: String,
     },
 }
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     match cli.command {
-        Commands::Run { cmd } => {
-            println!("would run: {:?}", cmd);
+        Commands::Capture { contexts, format } => {
+            println!("Contexts to be captured: {:?}", contexts);
+            println!("Output format: {:?}", format);
         }
     }
     Ok(())
