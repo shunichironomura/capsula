f3223da66bc14e1c6b60e122e4c789b4ab6cc8d6
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-09-20 19:44:36 +0900

    Improve Run structure

--- crates/capsula-core/src/run.rs
+++ crates/capsula-core/src/run.rs
@@ -4,19 +4,65 @@ use serde::{Deserialize, Serialize, Serializer};
 use shlex::try_join;
 use std::collections::HashMap;
 use std::io::{self, Read, Write};
+use std::path::{Path, PathBuf};
 use std::process::{Command, ExitStatus, Stdio};
 use std::thread;
 use std::time::{Duration, Instant};
 use ulid::Ulid;
 
 #[derive(Debug, Clone, Deserialize)]
-pub struct Run {
+pub struct Run<Dir = PathBuf> {
     pub id: Ulid,
     pub name: String,
     pub command: Vec<String>,
+    pub run_dir: Dir,
 }
 
-impl Serialize for Run {
+pub type UnpreparedRun = Run<()>;
+pub type PreparedRun = Run<PathBuf>;
+
+impl<Dir> Run<Dir> {
+    pub fn timestamp(&self) -> DateTime<Utc> {
+        // Calculate start time from ULID timestamp
+        let dt: DateTime<Utc> = self.id.datetime().into();
+        dt
+    }
+
+    pub fn run_dir(&self, vault_dir: impl AsRef<Path>) -> PathBuf {
+        let timestamp = self.timestamp();
+        let date_str = timestamp.format("%Y-%m-%d").to_string();
+        let time_str = timestamp.format("%H%M%S").to_string();
+
+        // Prefix the run directory with time because
+        // folders are sorted in natural order, not in lexicographical order,
+        // For example, on macOS Finder, the order is:
+        // 1. 01K5K478KNQ2ZXZG68MWM1Z9X6
+        // 2. 01K5K4571FGKBFTTRJCG1J3DCZ
+        // which is not the correct chronological order.
+        // By adding time prefix, it will be sorted correctly.
+        let run_dir_name = format!("{}-{}--{}", time_str, self.name, self.id.to_string());
+        vault_dir.as_ref().join(date_str).join(&run_dir_name)
+    }
+}
+
+impl Run<()> {
+    pub fn setup_run_dir(
+        &self,
+        vault_dir: impl AsRef<std::path::Path>,
+    ) -> io::Result<Run<PathBuf>> {
+        setup_vault(&vault_dir)?;
+        let run_dir = self.run_dir(&vault_dir);
+        std::fs::create_dir_all(&run_dir)?;
+        Ok(Run {
+            id: self.id,
+            name: self.name.clone(),
+            command: self.command.clone(),
+            run_dir,
+        })
+    }
+}
+
+impl Serialize for Run<()> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
@@ -26,6 +72,21 @@ impl Serialize for Run {
         state.serialize_field("name", &self.name)?;
         state.serialize_field("command", &self.command)?;
         state.serialize_field("timestamp", &self.timestamp().to_rfc3339())?;
+        state.end()
+    }
+}
+
+impl Serialize for Run<PathBuf> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("Run", 5)?;
+        state.serialize_field("id", &self.id)?;
+        state.serialize_field("name", &self.name)?;
+        state.serialize_field("command", &self.command)?;
+        state.serialize_field("timestamp", &self.timestamp().to_rfc3339())?;
+        state.serialize_field("run_dir", &self.run_dir.to_string_lossy())?;
         state.end()
     }
 }
@@ -56,38 +117,7 @@ fn exit_code_from_status(status: ExitStatus) -> i32 {
     }
 }
 
-impl Run {
-    pub fn timestamp(&self) -> DateTime<Utc> {
-        // Calculate start time from ULID timestamp
-        let dt: DateTime<Utc> = self.id.datetime().into();
-        dt
-    }
-
-    pub fn run_dir(&self, vault_dir: impl AsRef<std::path::Path>) -> std::path::PathBuf {
-        let timestamp = self.timestamp();
-        let date_str = timestamp.format("%Y-%m-%d").to_string();
-        let time_str = timestamp.format("%H%M%S").to_string();
-
-        // Prefix the run directory with time because
-        // folders are sorted in natural order, not in lexicographical order,
-        // For example, on macOS Finder, the order is:
-        // 1. 01K5K478KNQ2ZXZG68MWM1Z9X6
-        // 2. 01K5K4571FGKBFTTRJCG1J3DCZ
-        // which is not the correct chronological order.
-        // By adding time prefix, it will be sorted correctly.
-        let run_dir_name = format!("{}-{}--{}", time_str, self.name, self.id.to_string());
-        vault_dir.as_ref().join(date_str).join(&run_dir_name)
-    }
-
-    pub fn setup_run_dir(
-        &self,
-        vault_dir: impl AsRef<std::path::Path>,
-    ) -> std::io::Result<std::path::PathBuf> {
-        let run_dir = self.run_dir(&vault_dir);
-        std::fs::create_dir_all(&run_dir)?;
-        Ok(run_dir)
-    }
-
+impl Run<PathBuf> {
     pub fn exec(&self) -> std::io::Result<RunOutput> {
         if self.command.is_empty() {
             return Err(io::Error::new(io::ErrorKind::InvalidInput, "empty command"));
@@ -98,6 +128,10 @@ impl Run {
         let mut env_vars = HashMap::new();
         env_vars.insert("CAPSULA_RUN_ID", self.id.to_string());
         env_vars.insert("CAPSULA_RUN_NAME", self.name.clone());
+        env_vars.insert(
+            "CAPSULA_RUN_DIRECTORY",
+            self.run_dir.to_string_lossy().to_string(),
+        );
         env_vars.insert("CAPSULA_RUN_TIMESTAMP", self.timestamp().to_rfc3339());
         if let Ok(cmd_str) = try_join(self.command.iter().map(|s| s.as_str())) {
             env_vars.insert("CAPSULA_RUN_COMMAND", cmd_str);
@@ -163,4 +197,23 @@ impl Run {
             duration,
         })
     }
+}
+
+fn setup_vault(path: impl AsRef<std::path::Path>) -> io::Result<()> {
+    let path = path.as_ref();
+    if path.exists() {
+        return Ok(());
+    }
+    std::fs::create_dir_all(path)?;
+
+    // Place a .gitignore file to ignore all contents
+    let gitignore_path = path.join(".gitignore");
+    std::fs::write(
+        gitignore_path,
+        "\
+# Automatically generated by Capsula
+*",
+    )?;
+
+    Ok(())
 }
